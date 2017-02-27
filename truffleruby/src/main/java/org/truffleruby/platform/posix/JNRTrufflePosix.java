5a29403cae82d805d5320d7c4851995298421670
Benoit Daloze <eregontp@gmail.com>
2017-02-27 16:52:47 +0100

    Name posix().execve() appropriately
    * Remove unused variant.

--- truffleruby/src/main/java/org/truffleruby/platform/posix/JNRTrufflePosix.java
+++ truffleruby/src/main/java/org/truffleruby/platform/posix/JNRTrufflePosix.java
@@ -79,13 +79,7 @@ public class JNRTrufflePosix implements TrufflePosix {
 
     @TruffleBoundary
     @Override
-    public int exec(String path, String... argv) {
-        return posix.execv(path, argv);
-    }
-
-    @TruffleBoundary
-    @Override
-    public int exec(String path, String[] argv, String[] envp) {
+    public int execve(String path, String[] argv, String[] envp) {
         return posix.execve(path, argv, envp);
     }
 
